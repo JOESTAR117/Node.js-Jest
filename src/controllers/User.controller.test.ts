2a549e6c45b672c6d7349059766289842533212d
Jackson Rios <juniorrios1231@gmail.com>
2023-02-15 20:04:56 -0300

    AAA test passed✔

--- src/controllers/User.controller.test.ts
+++ src/controllers/User.controller.test.ts
@@ -1,5 +1,11 @@
 describe('Users Controlles', () => {
     it('Deve somar 1 + 1', () => {
-        expect(1+1).toEqual(2)
+        function soma(a: number, b: number) {
+            return a + b
+        }
+
+        const result = soma(1, 2)
+
+        expect(result).toEqual(3)
     })
 })
