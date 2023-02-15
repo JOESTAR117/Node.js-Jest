c9a6120a39481e2c3db7906caffa7416f7dee941
Jackson Rios <juniorrios1231@gmail.com>
2023-02-15 20:32:15 -0300

    feat: test created and passed ✔

--- src/controllers/User.controller.test.ts
+++ src/controllers/User.controller.test.ts
@@ -1,11 +1,42 @@
+import { makeMockResponse } from '../mocks/mockResponse'
+import { Request } from 'express'
+import { UsersController } from './User.controller'
+import { database } from '../database/user'
+
 describe('Users Controlles', () => {
-    it('Deve somar 1 + 1', () => {
-        function soma(a: number, b: number) {
-            return a + b
+    const usersController = new UsersController()
+
+    const mockRequest = {} as Request
+    const mockResponse = makeMockResponse()
+    it('Deve listar todos os usuarios', () => {
+        usersController.listarUsuario(mockRequest, mockResponse)
+
+        expect(mockResponse.state.status).toBe(200)
+        expect(mockResponse.state.json).toEqual(database)
+    })
+
+    it('Deve criar um novo usuario', () => {
+        mockRequest.body = {
+            name: 'Jackson Rios',
         }
 
-        const result = soma(1, 2)
+        usersController.criarUsuario(mockRequest, mockResponse)
 
-        expect(result).toEqual(3)
+        expect(mockResponse.state.status).toBe(201)
+        expect(mockResponse.state.json).toMatchObject({
+            mensagem: `Usuário Jackson Rios criado`,
+        })
+    })
+
+    it('Não deve criar um usuario com nome em branco', () => {
+        mockRequest.body = {
+            name: '',
+        }
+        usersController.criarUsuario(mockRequest, mockResponse)
+
+        expect(mockResponse.state.status).toBe(403)
+        expect(mockResponse.state.json).toMatchObject({
+            mensagem: 'Não é possível criar usuários sem um nome',
+        })
     })
 })
